a0976e0a6b03ad9cbe99ac19c6bef6f28b0ecb81
Tommy Wray <Tommy_04@hotmail.co.uk>
2019-08-08 13:31:16 +0100

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
 # code your #valid_move? method here
-
+valid_move?(board)
+def board
+    if position_taken? = false
+        
+    else
+        execute something else
+    end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
